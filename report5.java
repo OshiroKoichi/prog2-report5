80c56cd9e82ebf48222d15ba3b6312a11421859c
tameikip <e235707@ie.u-ryukyu.ac.jp>
2023-11-30 14:21:11 +0900

    ステップ2

--- report5.java
+++ report5.java
@@ -2,6 +2,11 @@ public class report5{
     
     public static void main(String[] args) {
         String str = null;
+        try{
         System.out.println(str.length());
+        } catch(Exception e){
+            System.out.println("NullPointerExceptionが発生しました。");
+            System.out.println(e.getMessage());
+        }
     }
 }
